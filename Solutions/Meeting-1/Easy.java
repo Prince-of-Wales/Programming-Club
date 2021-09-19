3a473cc68997c0c4b9232b7b284f72f3990a65cf
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-09-18 17:37:09 -0700

    Create Easy.java

---
+++ Solutions/Meeting-1/Easy.java
@@ -0,0 +1,10 @@
+Class Easy {
+  public static void main(String[] args) {
+    int i = /*input*/; 
+    if (i % 2 == 0) { 
+      System.out.println("The input is even.") 
+    } else { 
+      System.out.println("The input is odd.") 
+      }
+  }
+}
