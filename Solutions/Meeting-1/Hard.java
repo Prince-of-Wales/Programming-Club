9efaab78c5cac0e262ff99e5dfc207812af7135b
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-09-18 17:40:30 -0700

    Create Hard.java

---
+++ Solutions/Meeting-1/Hard.java
@@ -0,0 +1,11 @@
+Class Hard {
+  public static void main(String[] args) {
+    boolean one = /*input*/;
+    boolean two = /*input*/;
+    if (one != two) {
+      System.out.println("True");
+    } else {
+      System.out.println("False");
+    }
+  }
+}
