96cd81cede293aaf716f6d991ab51d786392b078
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-09-18 17:52:39 -0700

    Create Medium.java

---
+++ Solutions/Meeting-2/Medium.java
@@ -0,0 +1,16 @@
+// Using a “while” loop: Add numbers starting from 0 to “a number” such that the sum of all integers between the two are greater than 200. Print the number. 
+
+class Medium {
+  public static void main(String[] args) {
+    int i = 0; 
+    int sum = 0; 
+    while (sum < 200) { 
+      sum += i; 
+      i++; 
+    }
+    System.out.println(i); 
+  }
+}
+
+
+// Answer: 21
