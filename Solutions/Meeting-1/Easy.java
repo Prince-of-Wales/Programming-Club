241c1419e6155608eb8d83a8a3623d5b5b0c4eda
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-09-30 11:29:24 -0700

    Create Easy.java

---
+++ Solutions/Meeting-1/Easy.java
@@ -0,0 +1,10 @@
+// Create a String with value hello world and with name myString, create an Integer with value 12342 and with name firstInt. Print both variables.
+
+public class Easy {
+  public static void main(String[] args) {
+    String myString = "hello world";
+    int firstInt = 12342;
+    System.out.println(myString);
+    System.out.println(firstInt);
+  }
+}
