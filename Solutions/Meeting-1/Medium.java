43eb9851ec9ed4bce9c4991a429842e5c949dd35
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-09-30 11:38:36 -0700

    Create Medium.java

---
+++ Solutions/Meeting-1/Medium.java
@@ -0,0 +1,11 @@
+// Create a String with name firstName and value Bob. A second String variable with name lastName and with value Bracken. Create an integer variab with name age and value 43. 
+// Make a print statement that prints: Name: firstName's value, lastName's Value Age: age 
+
+public class Medium {
+  public static void main(String[] args) {
+    String firstName = "Bob";
+    String lastname = "Bracken";
+    int age = 43;
+    System.out.println("Name: "+firstname+", "+lastName+" Age: "+age);
+  }
+}
