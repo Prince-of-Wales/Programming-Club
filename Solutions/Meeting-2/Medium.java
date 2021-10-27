c528f2c322ec37a0ee55b814ce1c50c10d62ae7a
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-10-26 20:44:55 -0700

    Update Medium.java

--- Solutions/Meeting-2/Medium.java
+++ Solutions/Meeting-2/Medium.java
@@ -1,9 +1,11 @@
 // Make an XOR code block.  Ex: https://bit.ly/3nNmLtI
+import java.util.Scanner;
 
 public class Medium {
   public static void main(String[] args) {
-    boolean one = /*input*/;
-    boolean two = /*input*/;
+    Scanner sc = new Scanner (System.in);
+    boolean one = sc.nextBoolean();
+    boolean two = sc.nextBoolean():
     if (one != two) {
       System.out.println("True");
     } else {
