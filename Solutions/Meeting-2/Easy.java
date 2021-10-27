c7c1f6c5b0a461f3de41ea25cb7c496c0a4f493e
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2021-10-26 20:39:58 -0700

    Update Easy.java

--- Solutions/Meeting-2/Easy.java
+++ Solutions/Meeting-2/Easy.java
@@ -1,8 +1,10 @@
 // Given an integer input, check if the input is even or not.
+import java.util.Scanner;
 
 public class Easy {
   public static void main(String[] args) {
-    int i = /*input*/; 
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
     if (i % 2 == 0) {
       System.out.println("The input is even.");
     } else {
