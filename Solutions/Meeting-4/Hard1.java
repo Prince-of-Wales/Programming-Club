c4deabd4fda69dcce62d400548663578020fdc86
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 10:56:35 -0800

    Create Hard1.java

---
+++ Solutions/Meeting-4/Hard1.java
@@ -0,0 +1,75 @@
+//https://dmoj.ca/problem/ccc18s2
+
+public class Hard1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[][] growth = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				growth[i][j] = in.nextInt();
+			}
+		}
+		
+		int min = growth[0][0];
+		int row = 0;
+		int column = 0;
+		
+		if (growth[0][n-1] < min) {
+			row = 0;
+			column = n-1;
+			min = growth[0][n-1];
+		}
+		
+		if (growth[n-1][0] < min) {
+			row = n-1;
+			column = 0;
+			min = growth[n-1][0];
+		}
+		
+		if (growth[n-1][n-1] < min) {
+			row = n-1;
+			column = n-1;
+			min = growth[n-1][n-1];
+		}
+		
+		if (row == 0 && column == 0) {
+			for (int i= 0; i < n; i++) {
+				for (int j = 0; j<n; j++) {
+					System.out.print(growth[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
+		
+		else if (row == 0 && column > 0) {
+			for (int i= n-1; i >= 0; i--) {
+				for (int j = 0; j<n; j++) {
+					System.out.print(growth[j][i] + " ");
+				}
+				System.out.println();
+			}
+		}
+		
+		else if (row > 0 && column > 0) {
+			for (int i= n-1; i >= 0; i--) {
+				for (int j = n-1; j>=0; j--) {
+					System.out.print(growth[i][j] + " ");
+				}
+				System.out.println();
+			}
+		}
+		
+		else {
+			for (int i= 0; i < n; i++) {
+				for (int j = n-1; j>=0; j--) {
+					System.out.print(growth[j][i] + " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+
+}
