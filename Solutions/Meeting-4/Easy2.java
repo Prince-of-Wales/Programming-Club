c13a26d43987286ae59586a955f2a1b119f1c55a
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 10:53:23 -0800

    Update Easy2.java

--- Solutions/Meeting-4/Easy2.java
+++ Solutions/Meeting-4/Easy2.java
@@ -1,6 +1,6 @@
 // Given a set of numbers, using an array and for-loop, find the sum of all the numbers.
 
-public Easy2 {
+public class Easy2 {
   public static void main(String[] args) {
     int[] sumArray = {1,72,43,4,5,26,7,8,19,10};
     int sum = 0;
