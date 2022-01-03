f093c9370cfa532a694f8abebbc2be82b9783622
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 11:18:41 -0800

    Update Medium1.java

--- Solutions/Meeting-4/Medium1.java
+++ Solutions/Meeting-4/Medium1.java
@@ -1,5 +1,16 @@
-// QUestion
+// Given an integer array, reverse the order of the array. Ex. 1, 2, 3, 4, 5 –> 5, 4, 3, 2, 1 Array: 31, 24, 76, 8, 9, 342, 546, 3 
+
+import java.util.Arrays; // used for checking the value. Not important to program's functionality
 
 public class Medium1 {
-
+  public static void main(String[] args) {
+    int[] myArray = {31, 24, 76, 8, 9, 342, 546, 3};
+    for (int i = 0; i < myArray.length / 2; i++) {
+      int temp = myArray[i];
+      myArray[i] = myArray[myArray.length - i - 1];
+      myArray[myArray.length - i - 1] = temp;
+    }
+    int[] check = {3, 546, 342, 9, 8, 76, 24, 31}; // could be removed
+    System.out.println(Arrays.equals(myArray, check)); // could be removed
+  }
 }
