364444a18249b188a4b5dac0213d5fbcadde1e23
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 10:32:28 -0800

    Update Easy.java

--- Solutions/Meeting-4/Easy.java
+++ Solutions/Meeting-4/Easy.java
@@ -1,20 +1,13 @@
-/* Initalize an Integer array to have the valaue 10, -4, 39, -999, -23, 16, 35, -5, 49, -8 
-Using a for-loop, iterate through the array and print whether the value is even or odd and greater or less than zero. 
+/* Initialize an Integer array to have the value: [10, -4, 39, -999, -23, 16, 35, -5, 49, -8]. 
+Using a for-loop, iterate through the array and print all the negative numbers.   
 */
 
 public class Easy {
   public static void main(String[] args) {
     int[] myArray = {10, -4, 39, -999, -23, 16, 35, -5, 49, -8};
     for (int i = 0; i < myArray.length; i++) {
-      if (myArray[i] % 2 == 0) {
-        System.out.println("Even");
-      } else {
-        System.out.println("Odd");
-      }
-      if (myArray[i] > 0) {
-        System.out.println("Greater than zero");
-      } else if (myArray[i] < 0) {
-        System.out.println("Less than zero");
+      if (myArray[i] < 0) {
+        System.out.println(myArray[i]);
       }
     }
   }
