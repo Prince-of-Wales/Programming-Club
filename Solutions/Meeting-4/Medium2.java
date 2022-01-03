be37d7bf9a8493ab9a7e6b9c1bcd7fbd33031b49
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 11:29:17 -0800

    Update Medium2.java

--- Solutions/Meeting-4/Medium2.java
+++ Solutions/Meeting-4/Medium2.java
@@ -4,14 +4,14 @@ public class Medium2 {
   public static void main(String[] args) {
     int[] myArray = {9, 3, 5, 9, 19, 294, 94, 287, 33, 87, 56, 37, 56, 42, 78, 89, 1, 5, 8, 4, 9, 89, 4, 74, 32, 29};
     int length = myArray.length; // used to make program more readable
-    int min;
-    int max;
+    int min = myArray[0];
+    int max = myArray[0];
     for (int i = 0; i < length; i++) {
       if (myArray[i] > max) {
         max = myArray[i];
       }
       if (myArray[i] < min) {
-        min = myArray[i]
+        min = myArray[i];
       }
     }
     System.out.println("Min: "+min);
