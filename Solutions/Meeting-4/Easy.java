c6e656cb0bdac2642da064d3c80e44f58d32c79e
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 10:18:24 -0800

    Update Easy.java

--- Solutions/Meeting-4/Easy.java
+++ Solutions/Meeting-4/Easy.java
@@ -1,1 +1,7 @@
+/* Initalize an Integer array to have the valaue 10, 4, 39, 999, 23, 16, 35, -5, 99, 8 
+Using a for-loop, iterate through the array and print whether the value is even or odd and greater or less than zero. 
+*/
 
+public Easy {
+  
+}
