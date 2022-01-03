f362ffea86da46c97f544fa43cca15b3657685bc
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 11:09:32 -0800

    Update Hard2.java

--- Solutions/Meeting-4/Hard2.java
+++ Solutions/Meeting-4/Hard2.java
@@ -1,5 +1,31 @@
-// Question
+//https://dmoj.ca/problem/ccc21s1
+
+import java.util.Scanner;
 
 public class Hard2 {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner (System.in);
+		int n = in.nextInt();
+		double[] heights =  new double [n+1];
+		double[] widths = new double [n];
+		double total = 0;
+		
+		for (int i = 0; i < n+1; i++) {
+			heights[i] = in.nextInt();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			widths[i] = in.nextInt();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			total += ((heights[i] + heights[i+1])*widths[i])/2;
+		}
+		
+		System.out.println(total);
+	}
+
 }
+
