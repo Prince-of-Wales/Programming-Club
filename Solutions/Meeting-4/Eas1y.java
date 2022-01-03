95e2e1559c3bf70cc00ac8895fe6a857a1f1d6da
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-03 10:55:21 -0800

    Update and rename Easy.java to Eas1y.java

--- Solutions/Meeting-4/Eas1y.java
+++ Solutions/Meeting-4/Eas1y.java
@@ -2,7 +2,7 @@
 Using a for-loop, iterate through the array and print all the negative numbers.   
 */
 
-public class Easy {
+public class Easy1 {
   public static void main(String[] args) {
     int[] myArray = {10, -4, 39, -999, -23, 16, 35, -5, 49, -8};
     for (int i = 0; i < myArray.length; i++) {
