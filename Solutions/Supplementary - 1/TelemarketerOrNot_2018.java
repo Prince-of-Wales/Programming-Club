37abd78ddc48ff5d2cb20e26b9d1d66affad7417
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-01-19 17:21:18 -0800

    Update TelemarketerOrNot_2018.java

--- Solutions/Supplementary - 1/TelemarketerOrNot_2018.java
+++ Solutions/Supplementary - 1/TelemarketerOrNot_2018.java
@@ -1,14 +1,16 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class TelemarketerOrNot_2018 {
-  public static void main(String[] args) {
-    Scanner in = new Scanner (System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner (System.in);
 		int first = in.nextInt();
 		int second = in.nextInt();
 		int third = in.nextInt();
 		int fourth = in.nextInt();
 		boolean ignore = false;
-		
+			
 		if (first == 8 || first == 9) {
 			if (second == third) {
 				if (fourth == 8 || fourth == 9) {
@@ -17,10 +19,10 @@ public class TelemarketerOrNot_2018 {
 				}
 			}
 		}
-		
-		if(!ignore) {
+			
+		if (!ignore) {
 			System.out.println("answer");
 		}
-    
-    }
+	}
+
 }
