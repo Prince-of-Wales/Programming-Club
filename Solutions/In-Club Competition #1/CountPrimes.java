0e42f195b7450020546b0abbcf9e9f6ae4f440e1
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-05-24 22:48:21 -0700

    Update CountPrimes.java

--- Solutions/In-Club Competition #1/CountPrimes.java
+++ Solutions/In-Club Competition #1/CountPrimes.java
@@ -4,22 +4,22 @@ public class CountPrimes {
     int n = sc.nextInt();
     
     boolean[] notPrimes = new boolean [n];
-		if (n <= 2) System.out.println(0);
+    if (n <= 2) System.out.println(0);
 		
     else {
-      int count = 1; //initializing that 2 is prime
-		
-		  for (int i = 3; i < n; i += 2) { //skipping all even numbers
-			  if (!notPrimes[i]) {
-				  count++;
+      	int count = 1; //initializing that 2 is prime
+	
+	for (int i = 3; i < n; i += 2) { //skipping all even numbers
+		if (!notPrimes[i]) {
+			count++;
 				
-				  for (int j = 1; j*i < n; j++) { //all multiples of the prime number are not primes
-					  notPrimes[j*i] = true;
-				  }
-			  }
-		  }
+			for (int j = 1; j*i < n; j++) { //all multiples of the prime number are not primes
+				notPrimes[j*i] = true;
+			}
+		}
+	}
 		
-		  System.out.println(count);
+	System.out.println(count);
     } 
   }
 }
