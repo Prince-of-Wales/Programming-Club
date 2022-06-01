82b8ef40e473696b750bcdc0dcfd54c13400e0c6
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-05-31 21:56:40 -0700

    Create MoneyChange.java

---
+++ Solutions/In-Club Competition #1/MoneyChange.java
@@ -0,0 +1,54 @@
+public class MoneyChange {
+    	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] bills = new int[n];
+		boolean isPossible = true;
+		
+		for (int i = 0; i < n; i++) {
+			bills[i] = sc.nextInt();
+		}
+		
+		int tenDollars = 0;
+        int fiveDollars = 0;
+        
+        for (int i = 0; i < bills.length; i++) {
+        	if (bills[i] == 5) {
+        		fiveDollars ++;
+        	}
+        	
+        	else if (bills[i] == 10) {
+        		tenDollars ++;
+        		if (fiveDollars > 0) {
+        			fiveDollars --;
+        		}
+        		
+        		else {
+        			isPossible = false;
+        			break;
+        		}
+        	}
+        	
+        	//15 give back -> 10 & 5 || 5 & 5 & 5
+        	else if (bills[i] == 20) {  
+        		
+        		if (tenDollars > 0 && fiveDollars > 0) {
+        			tenDollars --;
+        			fiveDollars --;
+        		}
+        		
+        		else if (tenDollars <= 0 && fiveDollars >= 3) {
+        			fiveDollars -= 3;
+        		}
+        		
+        		else {
+        			isPossible = false;
+        			break;
+        		}
+        	}
+        }
+        
+        System.out.println(isPossible);
+	} 
+}
