f78ed273aabef01c994558a33c1131193cef16e3
Prince-of-Wales <90992407+Prince-of-Wales@users.noreply.github.com>
2022-05-31 21:02:54 -0700

    Updating variable names
    and changing a little bit of the code

--- Solutions/In-Club Competition #1/CountPrimes.java
+++ Solutions/In-Club Competition #1/CountPrimes.java
@@ -3,25 +3,27 @@ import java.util.Scanner;
 public class CountPrimes {
 	public static void main(String[] args) {
 	    Scanner sc = new Scanner(System.in);
-	    int n = sc.nextInt();
+	    int num = sc.nextInt();
     
-	    boolean[] notPrimes = new boolean [n];
-	    if (n <= 2) System.out.println(0);
+	    boolean[] notPrimes = new boolean [num];
+	    if (num <= 2) {
+		    System.out.println(0);
+	    }
 		
 	    else {
     		// initializing 2 as a prime
 		int count = 1; 
 		//skipping all even numbers
-		for (int i = 3; i < n; i += 2) { 
+		for (int i = 3; i < num; i += 2) { 
 			if (!notPrimes[i]) {
 				count++;
 				//all multiples of the prime number are not primes - set them to not prime
-				for (int j = 1; j*i < n; j++) { 
+				for (int j = 1; j*i < num; j++) { 
 					notPrimes[j*i] = true;
 				}
 			}
 		}
-		
+
 		System.out.println(count);
 	    } 
 	  }
